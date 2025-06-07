87d78f579ed40b29b5d27adef7336ee983c0dd4e
forrisdahl <nathanif@stud.ntnu.no>
2025-06-07 13:01:00 +0200

    feat: removed the status byte in encoder array, instead a failure to read is indicated by the angle being 0xFFFF

--- can_interface/src/can_interface_message_handler.cpp
+++ can_interface/src/can_interface_message_handler.cpp
@@ -7,19 +7,10 @@
 
   
 void encoder_angles_handler(const CANFD_Message& msg, rclcpp::Publisher<sensor_msgs::msg::JointState>::SharedPtr joint_state_pub, rclcpp::Clock::SharedPtr clock){
-            const uint8_t encoder_status = msg.data[6];
             std::vector<double> encoder_angles;
             convert_angles_to_radians(msg.data, encoder_angles);
 
-            for (int i = 0; i < NUM_ANGLES; i++) {
-                if (encoder_status & (1 << i)) {
-                    encoder_angles[i] = -1;
-                    spdlog::error("Reading encoder {} failed", i);
-                }
-            }
-
             auto joint_state_msg = sensor_msgs::msg::JointState();
-
             joint_state_msg.header.stamp = clock->now();
             joint_state_msg.name = {"shoulder", "wrist", "grip"};
             joint_state_msg.position = encoder_angles;
