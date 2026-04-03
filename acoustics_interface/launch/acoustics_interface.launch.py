c26fa6d1d75f816afcd5cf33661052faedb27f94
forrisdahl <nathanif@stud.ntnu.no>
2026-04-03 10:53:53 +0200

    fix: launch file not launching

--- acoustics_interface/launch/acoustics_interface.launch.py
+++ acoustics_interface/launch/acoustics_interface.launch.py
@@ -5,8 +5,8 @@ from launch_ros.actions import Node
 def generate_launch_description() -> LaunchDescription:
     acoustics_node = Node(
         package="acoustics_interface",
-        executable="acoustics_ros_node",
-        name="acoustics_ros_node",
+        executable="acoustics_interface",
+        name="acoustics_interface",
         namespace="nautilus",
         output="screen",
     )
